bf3e78504e382b32bf5f7fe6616d7eeda622dfd3
Richard Vodden <richard@vodden.com>
2025-01-08 19:39:51 +0000

    Ensure all CPUs are used during build

--- buildall.sh
+++ buildall.sh
@@ -19,11 +19,11 @@ echo "Installing into: $INSTALL_DIR"
 # configure and build gcc & friends
 cd riscv-gnu-toolchain
 ./configure --prefix=$INSTALL_DIR --with-arch=rv32i --with-multilib-generator="rv32i-ilp32--;rv32ima-ilp32--;rv32imafd-ilp32--"
-make
+make -j
 
 
 # configure and build qemu
 cd ../qemu
 ./configure --target-list=riscv32-softmmu --prefix=$INSTALL_DIR
-make
+make -j
 make install
